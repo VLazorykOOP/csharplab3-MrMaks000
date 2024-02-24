4b41ffca4e6a7ae61c51dda5860fe630ea88e996
Maks <rusnak.maksym@chnu.edu.ua>
2024-02-24 09:18:44 +0200

    2

---
+++ Lab3CSharp/IsoscelesTriangle.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+
+namespace Lab3CSharp
+{
+    internal class IsoscelesTriangle
+    {
+        private double side;
+        private double basis; 
+        private Color color; 
+
+        public IsoscelesTriangle(double side, double basis, Color color)
+        {
+            this.side = side;
+            this.basis = basis;
+            this.color = color;
+        }
+
+        public IsoscelesTriangle(int side, int b) : this(side, b, Color.White) { }
+
+        public double A { get { return side; } set { side = value; } }
+
+        public double B { get { return basis; } set { basis = value; } }
+
+        public Color Color { get { return color; } }       
+
+        public double Perimeter()
+        {
+            return 2 * side + basis;
+        }
+
+        public double Area()
+        {
+            return 0.5 * basis * Math.Sqrt((side * side) - ((basis * basis) / 4));
+        }
+
+        public bool RightTriangle()
+        {
+            return side == basis;
+        }
+
+        public void DisplayFields()
+        {
+            Console.WriteLine($"Side: {side}");
+            if (RightTriangle() == false)
+                Console.WriteLine($"Basis: {basis}");
+            Console.WriteLine($"Color: {color.Name}");
+        }
+    }
+}
--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Lab3CSharp
+﻿using System.Drawing;
+
+namespace Lab3CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Lab 3 ");
+
+            IsoscelesTriangle[] triangles = new IsoscelesTriangle[]
+            {
+                new IsoscelesTriangle(5, 4),
+                new IsoscelesTriangle(3, 3),
+                new IsoscelesTriangle(3, 2, Color.Aqua)
+            };
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                Console.WriteLine($"\nTriange {i}");
+                triangles[i].DisplayFields();
+                Console.WriteLine($"Perimeter: {triangles[i].Perimeter()}");
+                Console.WriteLine($"Area: {triangles[i].Area():F2}");
+                Console.WriteLine($"Right triangle: {triangles[i].RightTriangle()}");
+            }
         }
     }
 }
